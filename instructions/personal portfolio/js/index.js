06f9c34ad568472801d058d112d7569a64b97650
rfswofford <rebecca.swofford42@gmail.com>
2022-09-28 22:06:17 -0400

    added form and message section

--- instructions/personal portfolio/js/index.js
+++ instructions/personal portfolio/js/index.js
@@ -28,3 +28,37 @@ for (let i=0; i< skills.length; i++){
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+//selecting message form
+const messageForm = document.querySelector('[name="leave_message"]');
+
+//submitting form event
+const submission= messageForm.addEventListener('submit',formSubmit)
+
+//callback function for event listener which takes entry date from form, and creates a new list item under messages and adds a remove button
+function formSubmit(evt) {
+    evt.preventDefault();
+    const name= evt.target[0].value;
+    const email= evt.target[1].value;
+    const message= evt.target[2].value;
+    console.log(name + email + message);
+
+    const messageSection= document.querySelector('#messages'); 
+    const messageList= messageSection.querySelector ('ul'); 
+    let newMessage = document.createElement('li'); 
+    newMessage.innerHTML = `<a href="mailto:${email}"> ${name}</a> <span> ${message}</span>`;
+    let removeButton = document.createElement('button'); 
+    removeButton.innerText= 'Remove'; 
+    removeButton.type= 'button'; 
+    removeButton.addEventListener('click', buttonRemoval); 
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    document.querySelector ("[name='leave_message'").reset();
+}
+
+
+//function to remove button
+function buttonRemoval (evt){
+    evt.target.parentNode.remove(); 
+    
+}
